2e116a32546e932cb0d052873ade4bd2835d1c9f
CompSciHigh24 <csh-github@compscihigh.org>
2024-08-12 12:36:12 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const prompt = require('prompt-sync')()
+
+
+
+
+
+let competencies=["if/else", "while,","for", "functions", "objects"]
+
+for(let i= 0; i<= competencies ; i++){
+	console.log(i)
+}
